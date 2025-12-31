ca58c895ea3906e99d310ba9be7e0a033c72a753
PedroLucas <pedrolucasgomessantanaf@gmail.com>
2025-12-31 16:05:02 -0300

    change id of some buttons for class, feat: delete default items from the list

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 const inputAddItem = document.querySelector("#input-add-item")
-const buttonAddItem = document.querySelector("#button-add-item")
+const buttonAddItem = document.querySelector(".button-add-item")
 
 const list = document.querySelector("ul")
 let listItems = document.querySelectorAll("li")
-const buttonDeleteItem = document.querySelectorAll("#button-delete-item")
+const buttonDeleteItem = document.querySelectorAll(".button-delete-item")
 const alertDeleteMensage = document.querySelector(".alert-mensage")
 const closeAlertMensage = document.querySelector("#close-alert-mensage")
 
 function addAndDeleteItem() {
 
-    //Adicionar item a lista
+    //Adicionar item na lista
     buttonAddItem.addEventListener("click", () => {
         if (inputAddItem.value === "") {
             alert("Não foi possível adicionar o item à lista. Por favor, preencha o campo corretamente")
@@ -21,7 +21,7 @@ function addAndDeleteItem() {
             const deleteIcon = document.createElement("img")
 
 
-            deleteButton.setAttribute("id", "button-delete-item")
+            deleteButton.setAttribute("class", "button-delete-item")
             deleteButton.prepend(deleteIcon)
 
 
@@ -38,22 +38,28 @@ function addAndDeleteItem() {
 
             list.append(li);
 
-            //Deletar item da lista
-            buttonDeleteItem.forEach(element => {
-                element.addEventListener("click", () => {
-                    alertDeleteMensage.classList.remove("hide")
-                })
-
-                deleteButton.addEventListener("click", () => {
-                    alertDeleteMensage.classList.remove("hide")
-
-                    li.remove()
-                })
-
-                closeAlertMensage.addEventListener("click", () => {
-                    alertDeleteMensage.classList.add("hide")
-                })
+            deleteButton.addEventListener("click", () => {
+                alertDeleteMensage.classList.remove("hide")
+                li.remove()
             })
+
+            closeAlertMensage.addEventListener("click", () => {
+                alertDeleteMensage.classList.add("hide")
+            })
+
+
+
+            list.addEventListener("click", (event) => {
+                const deleteButton = event.target.closest(".button-delete-item");
+
+                if (!deleteButton) return;
+
+                const li = deleteButton.closest("li");
+
+                alertDeleteMensage.classList.remove("hide");
+                li.classList.add("hide");
+            });
+
         }
     })
 }
