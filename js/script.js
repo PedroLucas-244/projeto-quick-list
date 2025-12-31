d7fd4a9cf1bc1eaaab4030dabfe39cbd1c16e710
PedroLucas <pedrolucasgomessantanaf@gmail.com>
2025-12-31 15:27:49 -0300

    feat: add and delete items from the list

---
+++ js/script.js
@@ -0,0 +1,61 @@
+const inputAddItem = document.querySelector("#input-add-item")
+const buttonAddItem = document.querySelector("#button-add-item")
+
+const list = document.querySelector("ul")
+let listItems = document.querySelectorAll("li")
+const buttonDeleteItem = document.querySelectorAll("#button-delete-item")
+const alertDeleteMensage = document.querySelector(".alert-mensage")
+const closeAlertMensage = document.querySelector("#close-alert-mensage")
+
+function addAndDeleteItem() {
+
+    //Adicionar item a lista
+    buttonAddItem.addEventListener("click", () => {
+        if (inputAddItem.value === "") {
+            alert("Não foi possível adicionar o item à lista. Por favor, preencha o campo corretamente")
+        } else {
+            let li = document.createElement("li")
+            const input = document.createElement("input")
+            let liSpan = document.createElement("span")
+            const deleteButton = document.createElement("button")
+            const deleteIcon = document.createElement("img")
+
+
+            deleteButton.setAttribute("id", "button-delete-item")
+            deleteButton.prepend(deleteIcon)
+
+
+            deleteIcon.setAttribute("src", "./assets/icons/icone-lixeira.svg")
+
+
+            input.setAttribute("type", "checkbox")
+
+            liSpan = inputAddItem.value
+
+            li.prepend(input)
+            li.append(liSpan, deleteButton)
+            li.classList.add("list-item")
+
+            list.append(li);
+
+            //Deletar item da lista
+            buttonDeleteItem.forEach(element => {
+                element.addEventListener("click", () => {
+                    alertDeleteMensage.classList.remove("hide")
+                })
+
+                deleteButton.addEventListener("click", () => {
+                    alertDeleteMensage.classList.remove("hide")
+
+                    li.remove()
+                })
+
+                closeAlertMensage.addEventListener("click", () => {
+                    alertDeleteMensage.classList.add("hide")
+                })
+            })
+        }
+    })
+}
+
+addAndDeleteItem()
